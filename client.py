1f6a149d3ce396510c9e2d69df9d08518199e59a
Evgeny Umansky <euguman@gmail.com>
2020-12-30 01:03:30 +0200

    UDP + TCP - v1

--- client.py
+++ client.py
@@ -4,47 +4,92 @@ import scapy.all as scapy
 import struct
 
 # network ip
-ip = '127.0.0.1' # localhost
-# ip = scapy.get_if_addr('eth1') # dev
-# ip = scapy.get_if_addr('eth2') # test
+client_ip = '127.0.0.1' # localhost
+# client_ip = scapy.get_if_addr('eth1') # dev
+# client_ip = scapy.get_if_addr('eth2') # test
 
 # ports
 udp_port = 8081 # udp port on my localhost
 client_tcp_port = 9090 # tcp port on my localhost
-server_tcp_port = 0
 # udp_port = 13117 # udp port for sending offers
-# tcp_port = 2063 # tcp port we get from the course
+# client_tcp_port = 2063 # tcp port we get from the course
+server_tcp_ip = None
+server_tcp_port = None
 
+# our team name
+team_name = "Pink fluffy unicorns dancing on rainbow\n"
 
 
 def create_udp_connection_client():
     # Create a UDP socket
-    udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_server_socket.bind(('localhost', udp_port))
+    try:
+        udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # enable port reusage
+        udp_client_socket.bind((client_ip, udp_port))
+    except:
+        return
 
+    print("Client started, listening for offer requests...")
     while True:
         try:
-            data, server_address = udp_server_socket.recvfrom(1024)
-            
+            # recieve data and address of the server
+            data, server_address = udp_client_socket.recvfrom(1024)
+
+            # TODO: for debug, delete it
             print('received {} bytes from {}'.format(
                 len(data), server_address))
-
             print(data)
+
+            # get data from server in hex format
             server_data = struct.unpack('4s 1s 2s', data)
-            print(server_data)
-
-            magic_cookie = server_data[0].hex()
-            msg_type = server_data[1].hex()
-            server_tcp_port = server_data[2].hex()
-
-            if magic_cookie == 'feedbeef' and msg_type == '02':
+ 
+            magic_cookie = server_data[0].hex() # get magic cookie
+            msg_type = int(server_data[1].hex()) # get type of the message
+            
+            # validate the offer from the server
+            if magic_cookie == 'feedbeef' and msg_type == 2:
+                # get ip and port for tcp
+                global server_tcp_ip, server_tcp_port
+                server_tcp_ip = server_address[0] # get server's ip for tcp
+                server_tcp_port = int(server_data[2].hex()) # get server's port for tcp
                 break
+            
+        except:
+            pass
 
-        except Exception as err:
-            print(err)
-
-    udp_server_socket.close()
+    # close the udp stream
+    udp_client_socket.close()
     return
 
-print("Client started, listening for offer requests...")
+def create_tcp_connection_client():
+    if server_tcp_port is None or server_tcp_port is None:
+        return
+    
+    print("Received offer from {}, attempting to connect...".format(server_tcp_ip))
+
+    try:
+        # config tcp
+        tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create tcp socket
+        tcp_client_socket.connect((server_tcp_ip, server_tcp_port)) # connect the tcp socket to the server
+
+        # send the team name to the server
+        tcp_client_socket.sendall(team_name)
+
+        while True:
+            data = tcp_client_socket.recv(1024)
+            print('received {!r}'.format(data))
+
+    except Exception as e: 
+        print(e)
+    
+    finally:
+        # close the tcp udp stream
+        tcp_client_socket.close()
+    
+    global is_connected
+    is_connected = False
+    return
+
+
 create_udp_connection_client()
+create_tcp_connection_client()
--- server.py
+++ server.py
@@ -5,9 +5,12 @@ import struct
 
 
 # network ip
-ip = '127.0.0.1' # localhost
-# ip = scapy.get_if_addr('eth1') # dev
-# ip = scapy.get_if_addr('eth2') # test
+partly_ip = '127.0.0.' # localhost
+server_ip = '127.0.0.1'
+
+# partly_ip = '172.1.0.'
+# server_ip = scapy.get_if_addr('eth1') # dev
+# server_ip = scapy.get_if_addr('eth2') # test
 
 # ports
 udp_port = 8081 # udp port on my localhost
@@ -15,17 +18,15 @@ tcp_port = 8080 # tcp port on my localhost
 # tcp_port = 2008 # tcp port we get from the course
 # udp_port = 13117 # udp port for sending offers
 
-
-
 # create udp message
 magic_cookie = bytes.fromhex('feedbeef') # to hex byte string
 msg_type = bytes.fromhex('02') # to hex byte string
 server_port = bytes.fromhex(str(tcp_port)) # to hex byte string
 packed_message = struct.pack('4s 1s 2s', magic_cookie, msg_type, server_port) # 7 bytes for all
 
-# config tcp
+# open tcp port
 tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_server_socket.bind((ip, tcp_port)) # bind the tcp socket to ip of the machine and our tcp port
+tcp_server_socket.bind((server_ip, tcp_port)) # bind the tcp socket to ip of the machine and our tcp port
 tcp_server_socket.listen(1) # listen for incoming connections, only 1 allowed to be unaccepted
 
 players = {}
@@ -35,23 +36,62 @@ def create_udp_connection_server():
     udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # config udp
+    udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # enable port reusage
     udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # enable broadcasting mode for the udp socket
     udp_server_socket.bind(('', udp_port)) # bind the udp socket to all available interfaces and our udp_port
+    # udp_server_socket.settimeout(0.1) # next read/write operation call will be in 0.1 second
 
     time_end_broadcast = time.time() + 10 # when to finish the sending of offers (10 secs)
-    address = (ip, udp_port)
-    while time.time() < time_end_broadcast:
-        try:
-            udp_server_socket.sendto(packed_message, address)
-        except Exception as err:
-            print(err)
+    
+    print("Server started, listening on IP address " + str(server_ip))
+
+    try:
+        while time.time() < time_end_broadcast: 
+            for i in range(0, 256):
+                try:
+                    client_ip = partly_ip + str(i) # to all IPs in the system
+                    address = (client_ip, udp_port)
+                    udp_server_socket.sendto(packed_message, address)
+                except:
+                    pass
+            time.sleep(1) # broadcasting every second
+    except:
+        pass
 
     udp_server_socket.close()
     return
 
 
+def create_tcp_connection_server():
+    # config tcp
+    try:
+        while True:
+            # Wait for a connection
+            print('waiting for a connection...')
+            client_connection, client_address = tcp_server_socket.accept()
+            try:
+                print('connection from', client_address)
 
-print("Server started, listening on IP address " + str(ip))
-create_udp_connection_server()
+                # Receive the data in small chunks and retransmit it
+                while True:
+                    data = client_connection.recv(1024)
+                    print('received {!r}'.format(data))
+                    if data:
+                        print('sending data back to the client')
+                        client_connection.sendall(data)
+                    else:
+                        print('no data from', client_address)
+                        break
+            except:
+                pass
 
-tcp_server_socket.close()
+    except:
+        pass
+
+    finally:
+        tcp_server_socket.close()
+    return
+
+while True:
+    create_udp_connection_server()
+    create_tcp_connection_server()
