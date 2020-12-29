aa07a4986bcaf50e5168481670e85d477fc15758
Evgeny Umansky <euguman@gmail.com>
2020-12-29 18:41:28 +0200

    UDP V 2

---
+++ client.py
@@ -0,0 +1,49 @@
+import socket
+import time
+import scapy.all as scapy
+import struct
+
+# network ip
+ip = '127.0.0.1' # localhost
+# ip = scapy.get_if_addr('eth1') # dev
+# ip = scapy.get_if_addr('eth2') # test
+
+# ports
+udp_port = 8081 # udp port on my localhost
+tcp_port = 9090 # tcp port on my localhost
+# udp_port = 13117 # udp port for sending offers
+# tcp_port = 2063 # tcp port we get from the course
+
+
+
+def create_udp_connection_client():
+    # Create a UDP socket
+    udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_client_socket.bind(('localhost', udp_port))
+
+    while True:
+        try:
+            data, server_address = udp_client_socket.recvfrom(1024)
+            
+            print('received {} bytes from {}'.format(
+                len(data), server_address))
+
+            print(data)
+            server_data = struct.unpack('4s 1s 2s', data)
+            print(server_data)
+
+            magic_cookie = server_data[0].hex()
+            msg_type = server_data[1].hex()
+            server_port = server_data[2].hex()
+
+            if magic_cookie == 'feedbeef' and msg_type == '02':
+                sent = udp_client_socket.sendto(b'Ron', ('', server_address))
+                print('sent {} bytes back to {}'.format(
+                    sent, server_address))
+        except Exception as err:
+            print(err)
+
+    udp_client_socket.close()
+    return
+
+create_udp_connection_client()
--- server.py
+++ server.py
@@ -6,34 +6,38 @@ import struct
 
 # network ip
 ip = '127.0.0.1' # localhost
-# dev_network_ip = scapy.get_if_addr('eth1')
-# test_network_ip = scapy.get_if_addr('eth2')
+# ip = scapy.get_if_addr('eth1') # dev
+# ip = scapy.get_if_addr('eth2') # test
 
 # ports
-tcp_port = 8080 # tcp port on my localhost
 udp_port = 8081 # udp port on my localhost
+tcp_port = 8080 # tcp port on my localhost
 # tcp_port = 2008 # tcp port we get from the course
 # udp_port = 13117 # udp port for sending offers
 
-# create udp and tcp sockets
-udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# config udp
-udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # enable broadcasting mode for the udp socket
-udp_server_socket.bind(('', udp_port)) # bind the udp socket to all available interfaces and our udp_port
 
 # create udp message
-str_message = 'feedbeef02'+ str(tcp_port)
-byte_message = bytes.fromhex(str_message) # to hex byte string
-packed_message = struct.pack('7s', byte_message) # to pack with 7 bytes
+magic_cookie = bytes.fromhex('feedbeef') # to hex byte string
+msg_type = bytes.fromhex('02') # to hex byte string
+server_port = bytes.fromhex(str(tcp_port)) # to hex byte string
+packed_message = struct.pack('4s 1s 2s', magic_cookie, msg_type, server_port) # 7 bytes for all
 
 # config tcp
+tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_server_socket.bind((ip, tcp_port)) # bind the tcp socket to ip of the machine and our tcp port
 tcp_server_socket.listen(1) # listen for incoming connections, only 1 allowed to be unaccepted
 
 
-def create_udp_connection(ip):
+def create_udp_connection_server():
+    players = {}
+    # create udp socket
+    udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # config udp
+    udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # enable broadcasting mode for the udp socket
+    udp_server_socket.bind(('', udp_port)) # bind the udp socket to all available interfaces and our udp_port
+
     time_end_broadcast = time.time() + 10 # when to finish the sending of offers (10 secs)
     address = (ip, udp_port)
     while time.time() < time_end_broadcast:
@@ -42,11 +46,15 @@ def create_udp_connection(ip):
         except Exception as err:
             print(err)
 
+        
+        data, client_address = udp_server_socket.recvfrom(1024)
+        print(data, client_address)
+    udp_server_socket.close()
     return
 
 
 
+print("Server started, listening on IP address " + str(ip))
+create_udp_connection_server()
 
-
-udp_server_socket.close()
 tcp_server_socket.close()
